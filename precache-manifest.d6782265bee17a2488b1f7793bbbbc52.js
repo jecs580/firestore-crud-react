ef30c5887a109a16f5d7f7136455cc2004a7caed
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-09-07 23:01:59 -0400

    Updates

---
+++ precache-manifest.d6782265bee17a2488b1f7793bbbbc52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a0924650ba7a1be6a0edb108c32c53",
+    "url": "/roca-papel-tijeras/index.html"
+  },
+  {
+    "revision": "e2860d7e7a4b5abffff0",
+    "url": "/roca-papel-tijeras/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dfa9e759e2520a491973",
+    "url": "/roca-papel-tijeras/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "e2860d7e7a4b5abffff0",
+    "url": "/roca-papel-tijeras/static/js/2.03ce9c7b.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/roca-papel-tijeras/static/js/2.03ce9c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa9e759e2520a491973",
+    "url": "/roca-papel-tijeras/static/js/main.305520f9.chunk.js"
+  },
+  {
+    "revision": "af8af70b3a9e59f20b6d",
+    "url": "/roca-papel-tijeras/static/js/runtime-main.fa2c3a86.js"
+  }
+]);
